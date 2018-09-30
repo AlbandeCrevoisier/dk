37ef76c063e0300336cfbb0565595d68002ef886
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-10-01 00:35:25 +0200

    Rewrite client to resolve concurrency issues. Now subs to & prints multiple chans.

--- lab/1/pubsub_client.go
+++ lab/1/pubsub_client.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
 	"github.com/gomodule/redigo/redis"
 )
 
-func main() {
-	c, err := redis.Dial("tcp", "localhost:6379")
-	if err != nil {
-		fmt.Println(err)
-		return
+/* Reading standard input is blocking, so it is read in a go routine. */
+func getChanName(c chan string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+ 		c <- scanner.Text()
 	}
-	defer c.Close()
-	psc := redis.PubSubConn{Conn: c}
-	psc.Subscribe("chan1")
+}
+
+/* Subscribe to and print the content of the given channel. */
+func subChan(chanName string, conn redis.Conn) {
+	psc := redis.PubSubConn{Conn: conn}
+	psc.Subscribe(chanName)
 	for {
 		switch r := psc.Receive().(type) {
 		case error:
-			fmt.Println("Error")
+			fmt.Println(r.Error())
 			return
+		case redis.Subscription:
+			fmt.Println("Subscribed to: " + r.Channel)
 		case redis.Message:
-			fmt.Printf("%s %s\n", r.Channel, r.Data)
+			fmt.Println(r.Channel + ":" + string(r.Data))
 		}
 	}
 }
 
+func main() {
+	c := make(chan string)
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	go getChanName(c)
+	for {
+		go subChan(<- c, conn)
+	}
+}
