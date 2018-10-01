21dbcc72b39f7142a4673ba145cc7578dbe437b3
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-10-01 13:35:52 +0200

    Add unsub to client, which is now rather complete.

--- lab/1/pubsub_client.go
+++ lab/1/pubsub_client.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"os"
 	"bufio"
 	"github.com/gomodule/redigo/redis"
 )
 
 /* Reading standard input is blocking, so it is read in a go routine. */
-func getChanName(c chan string) {
+func getChanName(c chan string, conn redis.Conn) {
 	scanner := bufio.NewScanner(os.Stdin)
+	psc := redis.PubSubConn{Conn: conn}
 	for {
 		scanner.Scan()
- 		c <- scanner.Text()
+ 		inText := scanner.Text()
+		w := strings.Split(inText, " ")
+		if strings.Compare(w[0], "sub") == 0 {
+			c <- w[1]
+		} else if strings.Compare(w[0], "unsub") == 0 {
+			psc.Unsubscribe(w[1])
+		} else {
+			fmt.Println("usage: [sub | unsub] <channel name>")
+		}
 	}
 }
 
@@ -26,7 +36,13 @@ func subChan(chanName string, conn redis.Conn) {
 			fmt.Println(r.Error())
 			return
 		case redis.Subscription:
-			fmt.Println("Subscribed to: " + r.Channel)
+			switch r.Kind {
+			case "subscribe":
+				fmt.Println("Subscribed to: " + r.Channel)
+			case "unsubscribe":
+				fmt.Println("Unsubscribed to: " + r.Channel)
+				return
+			}
 		case redis.Message:
 			fmt.Println(r.Channel + ":" + string(r.Data))
 		}
@@ -41,7 +57,7 @@ func main() {
 		return
 	}
 	defer conn.Close()
-	go getChanName(c)
+	go getChanName(c, conn)
 	for {
 		go subChan(<- c, conn)
 	}
