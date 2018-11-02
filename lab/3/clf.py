75bc3fdfcc23656d731c14cd0d316f33a85e2699
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-11-02 22:52:08 +0100

    Use random forest.

--- lab/3/clf.py
+++ lab/3/clf.py
@@ -3,6 +3,15 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
 
-train = pd.read_csv("train-set.csv")
-test = pd.read_csv("test-set.csv")
+train = pd.read_csv('train-set.csv')
+train_X = train.drop(['Id', 'Cover_Type'], axis = 1).values
+train_Y = train.Cover_Type.values
+test = pd.read_csv('test-set.csv')
+test_X = test.drop(['Id'], axis = 1).values
 
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(train_X, train_Y)
+
+pred = rf.predict(test_X)
+
+pd.DataFrame({'Id': test.Id.values, 'Cover_Type': pred}).to_csv('pred.csv', index=False)
