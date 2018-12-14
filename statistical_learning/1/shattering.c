6ddaf696d7faead0171b00686d7b7509b9b13090
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-12-14 21:15:44 +0100

    Add skel C file for lab 1.

---
+++ statistical_learning/1/shattering.c
@@ -0,0 +1,23 @@
+/* Estimating the shattering coefficient for an input space given by the
+ * cartesian product of R real lines.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* iter: number of iterations used to assess every linear hyperplane.
+ * n_start, n_end: set the sample size n.
+ * R: for R^2, R = 2.
+ */
+int
+shattering_coef(int iter, int n_start, int n_end, int R)
+{
+
+}
+
+int
+main(void)
+{
+	return 0;
+}
+
